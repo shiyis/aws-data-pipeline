b4c398ec516bac3fe611a20e2e61c2f9e6b39cf0
KenCatlee <115297306+KenCatlee@users.noreply.github.com>
2022-10-23 02:06:55 -0400

    Update main.tf

---
+++ tf/modules/sagemaker/main.tf
@@ -0,0 +1,27 @@
+#Create Sagemaker notebook instance
+resource "aws_sagemaker_notebook_instance" "Sagemaker-instance" {
+  name = "Data-Sagemaker"
+  role_arn =
+  instance_type =
+  tags = {
+    Name = "AWS Pipeline"
+  }
+}
+
+#CreateSagemaker repo w/ instance
+resource "aws_sagemaker_code_repository" "Sagemaker-Repo" {
+  code_repository_name = "Instance-repo"
+  git_config {
+    repository_url = ""
+  }
+}
+resource "aws_sagemaker_notebook_instance" "Sagemaker-instance" {
+  instance_type = ""
+  name          = "Data-Sagemaker"
+  role_arn      = ""
+  default_code_repository = ""
+
+  tags = {
+    Name =
+  }
+}
