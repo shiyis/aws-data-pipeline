dcb47a35a245fea3ea4b83b8ff66e14f007496c6
Patrick Robertson <patrick.robertson106@gmail.com>
2022-10-25 15:53:05 -0400

    Some more changes to eventbridge-sqs-lambda

--- tf/modules/s3-sqs-lambda/main.tf
+++ tf/modules/s3-sqs-lambda/main.tf
@@ -4,3 +4,78 @@
 
 # Important note: This is where we will create and attach the bucket policy, so anything that needs access to s3
 # needs to be included in the variables of this module
+
+
+#Create SQS
+resource "aws_sqs_queue" "queue" {
+  name                      = "SQS-Lambda"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.q.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.example.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+  fifo_queue                = false
+  delay_seconds             = 0
+  # max_message_size          = 
+  # message_retention_seconds = 
+  # receive_wait_time_seconds = 
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
+    maxReceiveCount     = 4
+  })
+
+  tags = {
+    Environment = " "
+  }
+}
+
+#SQS mapping for Lambda
+resource "aws_lambda_event_source_mapping" "sqsLambda" {
+  event_source_arn = aws_sqs_queue.queue.arn
+  function_name = aws_lambda_function.test_lambda.arn
+}
+
+#Create Lambda role
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+#Create Lambda function
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = " "
+  function_name = " "
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = " "
+}
---
+++ tf/modules/sagemaker/main.tf
@@ -0,0 +1,59 @@
+<<<<<<< Updated upstream
+#Create Sagemaker notebook instance
+resource "aws_sagemaker_notebook_instance" "Sagemaker-instance" {
+  name = "Data-Sagemaker"
+  role_arn = ""
+  instance_type = ""
+  tags = {
+    Name = "AWS Pipeline"
+  }
+}
+
+#OPTION 2
+#CreateSagemaker repo w/ instance
+resource "aws_sagemaker_code_repository" "Sagemaker-Repo" {
+  code_repository_name = "Instance-repo"
+  git_config {
+    repository_url = ""
+  }
+}
+resource "aws_sagemaker_notebook_instance" "Sagemaker-instance" {
+  instance_type = ""
+  name          = "Data-Sagemaker"
+  role_arn      = ""
+  default_code_repository = ""
+
+  tags = {
+    Name = ""
+  }
+}
+=======
+#Create Sagemaker notebook instance
+resource "aws_sagemaker_notebook_instance" "Sagemaker-instance" {
+  name = "Data-Sagemaker"
+  role_arn = ""
+  instance_type = ""
+  tags = {
+    Name = "AWS Pipeline"
+  }
+}
+
+#OPTION 2
+#CreateSagemaker repo w/ instance
+resource "aws_sagemaker_code_repository" "Sagemaker-Repo" {
+  code_repository_name = "Instance-repo"
+  git_config {
+    repository_url = ""
+  }
+}
+resource "aws_sagemaker_notebook_instance" "Sagemaker-instance" {
+  instance_type = ""
+  name          = "Data-Sagemaker"
+  role_arn      = ""
+  default_code_repository = ""
+
+  tags = {
+    Name = ""
+  }
+}
+>>>>>>> Stashed changes
